ae0d252f38be472a65f188ff47f9ccaff7f5e02d
NatiTesfay <netaneltes66@gmail.com>
2022-05-23 23:27:31 +0300

    22.05.2022--main.js

---
+++ main.js
@@ -0,0 +1,107 @@
+// function printRandomNumber() {
+//     var rndNum = Math.floor(Math.random()*10);
+//     console.log(rndNum);
+// }
+// printRandomNumber();
+
+
+// function printRandomNumber() {
+//     var RandomNumber = Math.floor(Math.random()*10);
+//     console.log(RandomNumber);
+// }
+// printRandomNumber();
+
+
+// function RandomNumber() {
+//     var RandomNumber = 5;
+//    var num = Math.floor(Math.random()*5);
+//    console.log(num)
+// }
+// RandomNumber();
+
+// function RandomNumber() {
+//     var RandomNumber = [];
+//     var num = Math.floor(Math.random());
+//         if (RandomNumber == 11) {
+//             console.log('הצלחה');
+//         }
+//         else(console.log('כישלון'));
+// }
+// RandomNumber();
+
+
+// function RandomNumber() {
+//     var rndNum = [];
+//     for (let i = 0; i < 10; i++) {
+//         rndNum.push(Math.floor(Math.random()*100));
+//     }
+//     console.log(rndNum);
+// }
+// RandomNumber();
+
+
+
+
+// function RandomNumber() {
+//     var rndNum = [];
+//     for (let i = 0; i < 10; i++) {
+//         rndNum.push(Math.floor(Math.random()*10));
+//     }
+//     console.log(rndNum);
+//     return rndNum;
+// }
+// RandomNumber();
+
+// function RandomNumb() {
+//     var rndNum = prompt();
+//     // var RandomNumb = prompt();
+//     for (let i = 0; i < 5; i++) {
+//         rndNum.push(Math.floor(Math.random()*5)); 41.לסיים
+//         rndNum = rndNum(Math.min());  
+//     }
+//      console.log(rndNum(Math.min()));
+// }
+// RandomNumb();
+
+// function RandomNumber() {
+//     var num = +prompt('enter number');
+//    var rndNum=(Math.floor(Math.random()*100));
+//         if (num%2==0) { 
+
+//             return Math.min(num,rndNum);
+// }   
+//       else{
+//            return Math.max(num,rndNum);
+//       }
+
+// }
+// RandomNumber();
+
+// function RandomNumber() {
+//     var rndNum =[(Math.floor(Math.random()*364))];
+//     console.log(rndNum);
+//     var num =[Math.floor(Math.random()*873)];
+//     console.log(num);
+// }
+// RandomNumber();
+
+// function RandomNumber() {
+//     var rndNum =[Math.floor(Math.random()*100)];
+//     console.log(rndNum);
+// }
+// RandomNumber();
+
+// function RandomNumber() {
+//     var rndUser = +prompt ('enter text')+Math.floor(Math.random());
+//     console.log(rndUser.toUppercase()); - לסיים 159
+// }
+// RandomNumber();
+
+function RandomFunc() {
+    var rndUser = prompt('name:,name:,age:,age:')+Math.floor(Math.random());
+     
+    
+}
+
+
+
